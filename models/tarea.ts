dfc58b32d16f4626e9117a974e0d5e0bfc9c1afc
Matias Carrillo Sanchez <Matiascarrillosanchez17@gmail.com>
2024-10-15 22:18:30 -0500

    typing errors

--- models/tarea.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-const { Schema } = mongoose;
-
-const tareaSchema = new Schema({
-    nombre: String, // String is shorthand for {type: String}
-    tarea: String,
-    status: Boolean,
-});
